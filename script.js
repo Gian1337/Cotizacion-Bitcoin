adad6a87904df024a1551065c1251102304fcdeb
gianluca carlini <gianiboca@hotmail.com>
2022-10-31 17:50:18 +0100

    Cotizacion Bitcoin v1.0

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const usdAmount = document.getElementById('usd-amount');
+
+fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(response => response.json())
+    .then(data => displayData(data));
+
+const displayData = data => {
+    const usd = data.bpi.USD.rate_float;
+    usdAmount.textContent = `$${usd} USD`
+}
